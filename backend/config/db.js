05fa0b73906553c0e1a8b47311bc6511693a2357
Sonu Kumar <sonunigam0382002@gmail.com>
2025-11-05 12:32:20 +0530

    mongoDb Atlas Added

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,8 +2,8 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    const connection = await mongoose.connect(process.env.MONGO_URI);
-    console.log(`MongoDB Connected: ${connection.connection.host}`);
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log(`MongoDB Connected:`);
   } catch (error) {
     console.error(`Error connecting to MongoDB: ${error.message}`);
     process.exit(1);
